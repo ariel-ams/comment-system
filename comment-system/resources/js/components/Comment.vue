b97f9445bf2fe96056d684551cdae687baf1c1f7
Ariel Medina <arielm92@gmail.com>
2022-03-30 09:13:39 -0300

    Create Comment.vue

---
+++ comment-system/resources/js/components/Comment.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <span>{{ comment.username }}</span>
+        <br />
+        <p>{{ comment.comment_text }}</p>
+        <hr />
+    </div>
+</template>
+
+<script>
+import Comment from "@/model/Comment";
+
+export default {
+    props: {
+        comment: {
+            type: Comment,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style></style>
