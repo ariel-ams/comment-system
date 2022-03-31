9026a61a65fb51bae9e8647801dcd08e2c048b2c
Ariel Medina <arielm92@gmail.com>
2022-03-31 01:54:35 -0300

    Comment constructor can take parent_id and nesting_level
    - They are added as default arguments

--- comment-system/resources/js/model/Comment.js
+++ comment-system/resources/js/model/Comment.js
@@ -1,7 +1,9 @@
 class Comment{
-    constructor(username, comment_text){
+    constructor(username, comment_text, parent_id = null, nesting_level = 0){
         this.username = username;
         this.comment_text = comment_text;
+        this.parent_id = parent_id;
+        this.nesting_level = nesting_level;
     }
 }
 
