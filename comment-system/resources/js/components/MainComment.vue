e782c7a41c773872ef9abcf7be295f877262ef70
Ariel Medina <arielm92@gmail.com>
2022-04-01 03:09:50 -0300

    Main comment implements Reply Button

--- comment-system/resources/js/components/MainComment.vue
+++ comment-system/resources/js/components/MainComment.vue
@@ -5,40 +5,21 @@
         <p>{{ currentComment.comment_text }}</p>
         <hr />
         <p v-if="currentComment.children_count">{{ currentComment.children_count }}</p>
-        <button @click="showReplyForm" v-if="currentComment.canHaveChild()" >Reply</button>
-        <template v-if="replayFormVisible">
-            <CommentForm @close="hideReplyForm" nested :parent="currentComment"></CommentForm>            
-        </template>
+        <ReplyButton :comment="currentComment"></ReplyButton>
     </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapGetters } from 'vuex'
 
 export default {
     components:{
-        CommentForm: () => import('@/components/CommentForm'),
-    },
-    data(){
-        return {
-            replayFormVisible: false
-        }
+        ReplyButton: () => import('@/components/ReplyButton'),
     },
     computed:{
         ...mapGetters('comments',[
             'currentComment'
         ])
-    },
-    methods:{
-        ...mapActions('comments', [
-            'loadChildren'
-        ]),
-        showReplyForm(){
-            this.replayFormVisible = true;
-        },
-        hideReplyForm(){
-            this.replayFormVisible = false;
-        }
     }
 };
 </script>
