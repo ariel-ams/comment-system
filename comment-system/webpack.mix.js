7a0d7a2ab02888b1881fe564b33be8bf1842e00f
Ariel Medina <arielm92@gmail.com>
2022-03-31 21:03:43 -0300

    Added tailwind and vue-router libraries

--- comment-system/webpack.mix.js
+++ comment-system/webpack.mix.js
@@ -1,5 +1,6 @@
 const mix = require('laravel-mix');
 const path = require('path');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,7 +14,11 @@ const path = require('path');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('./tailwind.config.js') ],
+      });
 
 mix.webpackConfig({
     resolve: {
